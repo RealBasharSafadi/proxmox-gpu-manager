8ac4dc0f8d5676e49460df2307731e72f1d74218
RealBasharSafadi <118638447+RealBasharSafadi@users.noreply.github.com>
2025-01-30 20:06:39 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,10 +7,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 echo "Updating system and installing dependencies..."
-apt update && apt install -y python3 python3-pip git
-
-echo "Installing Python packages..."
-pip3 install rich proxmoxer
+apt update && apt install -y python3 python3-pip git python3-rich python3-proxmoxer
 
 echo "Cloning the repository..."
 if [ ! -d "/opt/proxmox-gpu-manager" ]; then
